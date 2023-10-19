8b35fe50b3bbe9ae1f443e2b44823bcd4881b5c4
KLoCascio <KyndalLife@gmail.com>
2023-10-19 19:15:29 -0400

    error fixed with database

--- models/hero.js
+++ models/hero.js
@@ -7,16 +7,15 @@ const { Schema } = require('mongoose')
 const Hero = new Schema(
     {
         name: { type: String, require: true },
-        race: { type: Schema.Types.ObjectId, ref: 'Race'},
-        subrace: { type: Schema.Types.ObjectId, ref: 'Subrace' },
+        race: { type: Schema.Types.ObjectId, ref: 'races'},
         raceFeatures: { type: Schema.Types.ObjectId, ref: 'RFeatures' },
         class: { type: Schema.Types.ObjectId, ref: 'Class' },
         subclass: { type: Schema.Types.ObjectId, ref: 'Subclass' },
         classFeatures: { type: Schema.Types.ObjectId, ref: 'CFeatures'},
-        cantrips: { type: Schema.Types.ObjectId, ref: 'Cantrips'},
-        spells: { type: Schema.Types.ObjectId, ref: 'Spells'},
-        abilities: { type: Schema.Types.ObjectId, ref: 'Abilities'},
-        skills: { type: Schema.Types.ObjectId, ref: 'Skills' },
+        cantrips: { type: Schema.Types.ObjectId, ref: 'cantrips'},
+        spells: { type: Schema.Types.ObjectId, ref: 'spells'},
+        abilities: { type: Schema.Types.ObjectId, ref: 'abilities'},
+        skills: { type: Schema.Types.ObjectId, ref: 'skills' },
         background: { type: Schema.Types.ObjectId, ref: 'Background' },
 
     }
